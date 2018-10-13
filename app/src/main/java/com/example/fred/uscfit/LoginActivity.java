ac790f9e900d997de2a2b3374203fada10977f13
Zhixu Li <zhixul@usc.edu>
2018-10-13 15:12:46 -0700

    loginactivity

--- app/src/main/java/com/example/fred/uscfit/LoginActivity.java
+++ app/src/main/java/com/example/fred/uscfit/LoginActivity.java
@@ -195,7 +195,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private boolean isEmailValid(String email) {
         //TODO: Replace this with your own logic
 
-
         return email.contains("@");
     }
 
