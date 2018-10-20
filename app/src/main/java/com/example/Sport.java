16922ea47619899a55327c401700ea43d748ede2
Haozhe Chen <haozhech@usc.edu>
2018-10-20 15:32:04 -0700

    Deleted commented out lines in AddSportActivity

--- app/src/main/java/com/example/Sport.java
+++ app/src/main/java/com/example/Sport.java
@@ -1,6 +1,15 @@
 package com.example;
 
-public class Sport {
+import java.io.Serializable;
+
+public class Sport implements Serializable {
+    public Sport() {
+
+    }
+    public Sport(String name, int calorie) {
+        this.name = name;
+        this.calorie = calorie;
+    }
     public String name;
     public int calorie;
 }
--- app/src/main/java/com/example/fred/uscfit/AddSportActivity.java
+++ app/src/main/java/com/example/fred/uscfit/AddSportActivity.java
@@ -1,23 +1,29 @@
 package com.example.fred.uscfit;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.content.Intent;
 
 import com.example.Sport;
+import com.example.db.DBController;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public class AddSportActivity extends AppCompatActivity {
 
-    private Button addSportBtn;
-    private EditText sportNameInput;
-    private EditText caloriesInput;
+    private Button addSportBtn = null;
+    private EditText sportNameInput = null;
+    private EditText caloriesInput = null;
     private String sportName = "";
     private int calories = 0;
+    private DBController dbController = null;
+    private GetAllSportsTask mGetAllSportsTask = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,45 +31,101 @@ public class AddSportActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_add_sport);
 
-        Intent intent = getIntent();
+        final Intent intent = getIntent();
+        dbController = new DBController();
 
-        sportNameInput = (EditText) findViewById(R.id.sportNameText);
-        caloriesInput = (EditText) findViewById(R.id.caloriesText);
         addSportBtn = (Button) findViewById(R.id.addSportBtn);
         addSportBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                // check if sport name is empty
+                sportNameInput = (EditText) findViewById(R.id.sportNameText);
                 if(sportNameInput.getText().toString().length() == 0) {
-                    // empty sport name input
+                    alert("Oops...", "Please enter a sport category.");
                     return;
+                } else {
+                    sportName = sportNameInput.getText().toString();
                 }
+
+                // check if calories is empty
+                caloriesInput = (EditText) findViewById(R.id.caloriesText);
                 if(caloriesInput.getText().toString().length() == 0) {
-                    // empty calories input
+                    alert("Oops...", "Please enter the calories consumption rate.");
                     return;
+                } else {
+                    calories = Integer.parseInt(caloriesInput.getText().toString());
                 }
-                if(sportNameExists(sportName)) {
-                    // sport name already exists in database
 
-                }
-                // insert to database
-                calories = Integer.parseInt(caloriesInput.getText().toString());
-                sportName = sportNameInput.getText().toString();
-                // call DBController to insert sport
+                // Async task check if sport category already exists in database
+                mGetAllSportsTask = new GetAllSportsTask(getIntent().getStringExtra("email"), sportName, calories);
+                mGetAllSportsTask.execute((Void) null);
 
+                // clear input to wait for next input
+                sportNameInput.setText("");
+                caloriesInput.setText("");
             }
         });
 
     }
 
-    // check if the sport name already exists in database
-    boolean sportNameExists(String sportName) {
-        // query database
-        ArrayList<Sport> sports = null;
-        for(Sport sport : sports) {
-            if(sport.name.equals(sportName)) {
-                return true;
+    // generate alert box with customized title and message
+    private void alert(String title, String msg) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(msg)
+                .setTitle(title);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                return;
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+
+
+    public class GetAllSportsTask extends AsyncTask<Void, Void, Boolean> {
+
+        private final String mEmail;
+        private final String mSportName;
+        private final int mCalories;
+
+        GetAllSportsTask(String email, String sportName, int calories) {
+            mEmail = email;
+            mSportName = sportName;
+            mCalories = calories;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            // check if the sport already exists
+            List<Sport> mySports = dbController.getAllSports(mEmail);
+            if(mySports == null || mySports.size() == 0) return false;
+            for(Sport mySport : mySports) {
+                if(mySport.name.equals(mSportName)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            if (success) {
+                // this means sport already exists
+                alert("Oops...", "Sport category already exists.");
+                return;
+            } else {
+                // sport doesn't exist
+                // all input is valid, now call DBController to insert sport
+                dbController.addSports(getIntent().getStringExtra("email"), new Sport(sportName, calories));
+                alert("Yeah!", "Successfully added sport category.");
+                return;
             }
         }
-        return false;
+
     }
+
 }
