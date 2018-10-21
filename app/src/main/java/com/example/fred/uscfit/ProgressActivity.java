3ea293084e8e1fc4077f2153300299683da715a1
Muyao Xu <muyaoxu@usc.edu>
2018-10-21 16:46:30 -0700

    update plan completeness for current date

--- app/src/main/java/com/example/fred/uscfit/ProgressActivity.java
+++ app/src/main/java/com/example/fred/uscfit/ProgressActivity.java
@@ -1,14 +1,20 @@
 package com.example.fred.uscfit;
 
-import android.content.DialogInterface;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.AsyncTask;
-import android.support.v7.app.AlertDialog;
+import android.os.Build;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -18,11 +24,9 @@ import com.example.Plan;
 import com.example.db.DBController;
 import com.google.firebase.Timestamp;
 
-import java.sql.Time;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -32,10 +36,13 @@ public class ProgressActivity extends AppCompatActivity {
     private DBController dbController = null;
     private String mEmail;
     private Map<String, Plan> myPlans;
-    private Map<String, Activity> myActivities;
+    private Map<String, List<Activity>> myActivities;
     private Map<String, Footstep> myFootsteps;
     private Calendar cal;
-    SimpleDateFormat sdf;
+    private SimpleDateFormat sdf;
+
+    private View mProgressView;
+    private View mLoadingView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +55,8 @@ public class ProgressActivity extends AppCompatActivity {
         cal = Calendar.getInstance();
         cal.add(Calendar.DAY_OF_MONTH, -1);
         sdf = new SimpleDateFormat("yyyy_MM_dd");
+        mProgressView = findViewById(R.id.progressView);
+        mLoadingView = findViewById(R.id.loadingView);
 
 
         GetAllPlansTask mGetAllPlansTask = new GetAllPlansTask((mEmail));
@@ -84,7 +93,7 @@ public class ProgressActivity extends AppCompatActivity {
 
     public void updatePlanStatus() {
         SimpleDateFormat outputSdf = new SimpleDateFormat("yyyy MMM.dd");
-        String currDate = outputSdf.format(cal.getTime());
+        String currDate = sdf.format(cal.getTime());
 //        TextView tv2 = (TextView) findViewById(R.id.textViewPlan2);
 //        TextView tv3 = (TextView) findViewById(R.id.textViewPlan3);
 //        TextView tv4 = (TextView) findViewById(R.id.textViewPlan4);
@@ -93,24 +102,46 @@ public class ProgressActivity extends AppCompatActivity {
 //        TextView tv7 = (TextView) findViewById(R.id.textViewPlan7);
 
         boolean planCompleted = true;
-
-//        Plan plan = myPlans.get(currDate);
-
-
-
-
-//        TextView tv1 = (TextView) findViewById(R.id.textViewPlan1);
-//        tv1.setText(currDate);
-//        ImageView imageViewCheckOn = (ImageView) findViewById(R.id.imageView2);
-//        if(planCompleted) {
-//            imageViewCheckOn.setVisibility(View.VISIBLE);
-//        }
-//        else{
-//            imageViewCheckOn.setVisibility(View.GONE);
-//        }
+        Plan plan = myPlans.get(currDate);
+        for(Object o: plan.activity) {
+            if (o.getClass() == Activity.class) {
+                Activity plannedActivity = (Activity) o;
+                List<Activity> currActivities = myActivities.get(currDate);
+                boolean plannedActivityCompleted = false;
+                for (Activity activity : currActivities) {
+                    if (isFinishedActivity(activity, plannedActivity)) {
+                        plannedActivityCompleted = true;
+                        break;
+                    }
+                }
+                if (!plannedActivityCompleted) {
+                    planCompleted = false;
+                    break;
+                }
+            }
+        }
+        TextView tv1 = (TextView) findViewById(R.id.textViewPlan1);
+//            ImageView imageViewCheckOn = (ImageView) findViewById(R.id.imageView2);
+        if(planCompleted) {
+            tv1.setText(currDate + " completed");
+        }
+        else{
+            tv1.setText(currDate + " not completed");
+        }
 
 
     }
+
+    // return true is a fulfills plan b
+    public boolean isFinishedActivity(Activity a, Activity b) {
+        if(!a.name.equals(b.name)) return false;
+//        if a start later than b
+        if(a.start.compareTo(b.start) > 0) return false;
+//        if a ends earlier than b
+        if(a.end.compareTo(b.end) < 0) return false;
+        return true;
+    }
+
 
 
 
@@ -140,7 +171,7 @@ public class ProgressActivity extends AppCompatActivity {
             Log.d(TAG, "doInBackground: " + myPlans.size());
 
             List<Object> allActivities = dbController.getAllActivity(mEmail);
-            Map<String, Activity> myActivities = new HashMap<>();
+            Map<String, List<Activity>> myActivities = new HashMap<>();
             Map<String, Footstep> myFootsteps = new HashMap<>();
             for(Object o: allActivities) {
                 HashMap<String, Object> map = (HashMap<String, Object>) o;
@@ -158,7 +189,14 @@ public class ProgressActivity extends AppCompatActivity {
                     activity.start = (Timestamp)map.get("start");
                     activity.end = (Timestamp)map.get("end");
                     String currDate = sdf.format(activity.start.toDate());
-                    myActivities.put(currDate, activity);
+                    if(myActivities.containsKey(currDate)) {
+                        myActivities.get(currDate).add(activity);
+                    }
+                    else {
+                        List<Activity> newActivity = new ArrayList<>();
+                        newActivity.add(activity);
+                        myActivities.put(currDate, newActivity);
+                    }
                 }
             }
 
@@ -167,6 +205,7 @@ public class ProgressActivity extends AppCompatActivity {
             ProgressActivity.this.myFootsteps = myFootsteps;
 
             ProgressActivity.this.updateFootstepBar();
+            ProgressActivity.this.updatePlanStatus();
 
 
             return true;
@@ -187,6 +226,7 @@ public class ProgressActivity extends AppCompatActivity {
                 return;
             }
         }
+
 
     }
 }
