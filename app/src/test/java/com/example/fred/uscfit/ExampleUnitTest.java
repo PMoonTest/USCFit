ddbd5f3810a60ef5dd8a620b7a30102d3c36b5eb
Steven Xu <siyuanx@usc.edu>
2018-10-27 17:37:24 -0700

    Update README

--- app/src/test/java/com/example/fred/uscfit/ExampleUnitTest.java
+++ app/src/test/java/com/example/fred/uscfit/ExampleUnitTest.java
@@ -14,4 +14,9 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+    @Test
+    public void helper() {
+        AddPlanActivity activity = new AddPlanActivity();
+
+    };
 }
