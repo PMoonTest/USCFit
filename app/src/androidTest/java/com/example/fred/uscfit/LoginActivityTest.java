ee4f7309748396cbddda08c55b1716e1073b0426
Steven Xu <siyuanx@usc.edu>
2018-10-28 13:57:19 -0700

    Login Activity Test

--- app/src/androidTest/java/com/example/fred/uscfit/LoginActivityTest.java
+++ app/src/androidTest/java/com/example/fred/uscfit/LoginActivityTest.java
@@ -1,14 +1,15 @@
 package com.example.fred.uscfit;
 
+import android.support.test.annotation.UiThreadTest;
 import android.support.test.rule.ActivityTestRule;
-import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-
-import static org.junit.Assert.assertNotNull;
 
 public class LoginActivityTest {
 
@@ -16,16 +17,87 @@ public class LoginActivityTest {
     public ActivityTestRule<LoginActivity> mLoginActivity = new ActivityTestRule<LoginActivity>(LoginActivity.class);
 
     private LoginActivity mActivity = null;
+    private AutoCompleteTextView username = null;
+    private EditText password = null;
+    private Button loginButton = null;
 
     @Before
     public void setUp() throws Exception {
         mActivity = mLoginActivity.getActivity();
+        username = (AutoCompleteTextView) mActivity.findViewById(R.id.email);
+        password = (EditText) mActivity.findViewById(R.id.password);
+        loginButton = (Button) mActivity.findViewById(R.id.email_sign_in_button);
     }
 
     @Test
-    public void test() {
-        View view = mActivity.findViewById(R.id.email);
-        assertNotNull(view);
+    @UiThreadTest
+    public void testCorrectPassword() {
+        username.requestFocus();
+        username.setText("siyuanx@usc.edu");
+        password.requestFocus();
+        password.setText("13444");
+        loginButton.callOnClick();
+    }
+
+    @Test
+    @UiThreadTest
+    public void testIncorrectPassword() {
+        username.requestFocus();
+        username.setText("siyuanx@usc.edu");
+        password.requestFocus();
+        password.setText("12345678910123123123");
+        loginButton.callOnClick();
+    }
+
+    @Test
+    @UiThreadTest
+    public void testEmptyPassword() {
+        username.requestFocus();
+        username.setText("siyuanx@usc.edu");
+        password.requestFocus();
+        password.setText("");
+        loginButton.callOnClick();
+    }
+
+    @Test
+    @UiThreadTest
+    public void testEmptyUsername() {
+        username.requestFocus();
+        username.setText("");
+        password.requestFocus();
+        password.setText("13444");
+        loginButton.callOnClick();
+    }
+
+    @Test
+    @UiThreadTest
+    public void testInvalidEmail() {
+        username.requestFocus();
+        username.setText("siyuanxusc");
+        password.requestFocus();
+        password.setText("13444");
+        loginButton.callOnClick();
+    }
+
+    @Test
+    @UiThreadTest
+    // for password length < 4
+    public void testInvalidPassword() {
+        username.requestFocus();
+        username.setText("siyuanx@usc.edu");
+        password.requestFocus();
+        password.setText("123");
+        loginButton.callOnClick();
+    }
+
+    @Test
+    @UiThreadTest
+    public void testAddNewUser() {
+        username.requestFocus();
+        username.setText("newUser@usc.edu");
+        password.requestFocus();
+        password.setText("12345");
+        loginButton.callOnClick();
     }
 
     @After
--- app/src/test/java/com/example/fred/uscfit/ExampleUnitTest.java
+++ app/src/test/java/com/example/fred/uscfit/ExampleUnitTest.java
@@ -1,10 +1,6 @@
 package com.example.fred.uscfit;
 
-import com.example.db.DBController;
-
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -12,16 +8,14 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-    public DBController db = new DBController();
 
     @Test
     public void testAddNewUser() {
-        db.addNewUser("zhixul0927@gmail.com","12345678");
+
         //assertEquals(4, 2 + 2);
     }
     @Test
     public void helper() {
-        AddPlanActivity activity = new AddPlanActivity();
 
     };
 }
