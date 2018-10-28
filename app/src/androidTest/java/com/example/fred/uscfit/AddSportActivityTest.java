6fe85215250319f3b5cbe62557945d6cb789a099
Haozhe Chen <haozhech@usc.edu>
2018-10-28 16:46:34 -0700

    comment dbcontroller in unit test

--- app/src/androidTest/java/com/example/fred/uscfit/AddSportActivityTest.java
+++ app/src/androidTest/java/com/example/fred/uscfit/AddSportActivityTest.java
@@ -11,6 +11,8 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import static org.junit.Assert.assertTrue;
+
 public class AddSportActivityTest {
 
     @Rule
@@ -29,50 +31,41 @@ public class AddSportActivityTest {
         Intent intent = new Intent();
         intent.putExtra("email", mEmail);
         mActivity = mAddSportActivity.launchActivity(intent);
-        mAddSportBtn = mActivity.findViewById(R.id.addSportBtn);
-        mSportNameInput = mActivity.findViewById(R.id.sportNameText);
-        mCaloriesInput = mActivity.findViewById(R.id.caloriesText);
+        mAddSportBtn = (Button) mActivity.findViewById(R.id.addSportBtn);
+        mSportNameInput = (EditText) mActivity.findViewById(R.id.sportNameText);
+        mCaloriesInput = (EditText) mActivity.findViewById(R.id.caloriesText);
     }
 
     @Test
     @UiThreadTest
     public void emptySportNameTest() {
         // perform click on button without sport name input
+        assertTrue(mSportNameInput.getText().toString().length() == 0);
+        mAddSportBtn.performClick();
+    }
 
-//        mAddSportBtn.performClick();
-//
-//        mSportNameInput.requestFocus();
-//        mSportNameInput.setText("random");
-//        mAddSportBtn.performClick();
+    @Test
+    @UiThreadTest
+    public void emptyCaloriesTest() {
+        // perform click on button with sport name but without calories input
+        mSportNameInput.requestFocus();
+        mSportNameInput.setText("random");
+        assertTrue(mSportNameInput.getText().toString().equals("random"));
+        mAddSportBtn.performClick();
+    }
 
+    @Test
+    @UiThreadTest
+    public void correctInput() {
+        // perform click on button with sport name (not duplicate) and calories input
         mSportNameInput.requestFocus();
         mSportNameInput.setText("random");
         mCaloriesInput.requestFocus();
         mCaloriesInput.setText("100");
-        mAddSportBtn.performClick();
+        assertTrue(mSportNameInput.getText().toString().equals("random"));
+        assertTrue(mCaloriesInput.getText().toString().equals("100"));
+//        mAddSportBtn.performClick();
     }
-
-//    @Test
-//    @UiThreadTest
-//    public void emptyCaloriesTest() {
-//        initialization();
-//        // perform click on button with sport name but without calories input
-//        mSportNameInput.requestFocus();
-//        mSportNameInput.setText("random");
-//        mAddSportBtn.performClick();
-//    }
-//
-//    @Test
-//    @UiThreadTest
-//    public void correctInput() {
-//        initialization();
-//        // perform click on button with sport name (not duplicate) and calories input
-//        mSportNameInput.requestFocus();
-//        mSportNameInput.setText("random");
-//        mCaloriesInput.requestFocus();
-//        mCaloriesInput.setText("100");
-//        mAddSportBtn.performClick();
-//    }
 
     @After
     public void tearDown() throws Exception {
