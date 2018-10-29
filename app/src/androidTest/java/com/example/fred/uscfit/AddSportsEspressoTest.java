b6dbede8e7da3d863fdc910c49363ebec0afdf99
Zhixu Li <zhixul@usc.edu>
2018-10-28 18:38:27 -0700

    random string

--- app/src/androidTest/java/com/example/fred/uscfit/AddSportsEspressoTest.java
+++ app/src/androidTest/java/com/example/fred/uscfit/AddSportsEspressoTest.java
@@ -6,6 +6,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.Random;
+
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
@@ -41,7 +43,15 @@ public class AddSportsEspressoTest {
         Intent i = new Intent();
         i.putExtra("email","siyuanx@usc.edu");
         mActivityRule.launchActivity(i);
-        onView(withId(R.id.sportNameText)).perform(typeText("Running2"));
+        String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        StringBuilder salt = new StringBuilder();
+        Random rnd = new Random();
+        while (salt.length() < 18) { // length of the random string.
+            int index = (int) (rnd.nextFloat() * SALTCHARS.length());
+            salt.append(SALTCHARS.charAt(index));
+        }
+        String saltStr = salt.toString();
+        onView(withId(R.id.sportNameText)).perform(typeText(saltStr));
         onView(withId(R.id.caloriesText)).perform(typeText("300"));
         onView(withId(R.id.addSportBtn)).perform(click());
         //onView(withText("Successfully added sport category1."));
